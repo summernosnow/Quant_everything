7ad244096a350d0d956f60d105ce8c03c0e71f03
theshy6666666 <104809195+theshy6666666@users.noreply.github.com>
2025-03-06 11:40:58 +0800

    Delete CNN-Quantization/shell_scripts/integer_programming.sh

--- CNN-Quantization/shell_scripts/integer_programming.sh
+++
@@ -1,5 +0,0 @@
-python scripts/integer_programming.py --mixed_precision_type weight\
-                                      --sensitivity /root/yixiaojie/CNN-Quantization/quant_output_random/yolov10s6_w8a8/sensitivity_start.yaml\
-                                      --para_size_config cnn_weight_ratio/yolov10_weight_ratio.yaml\
-                                      --mixed_precision_config /root/yixiaojie/CNN-Quantization/quant_output_random/yolov10s6_w8a8/yolov10_w8a8\
-                                      --target_bitwidth 8  # 我们希望做到的混合精度平均位宽
